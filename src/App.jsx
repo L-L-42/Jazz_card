54808fa0dbfccc3152b108e209187500e3e5f5e6
Luc <lefebureluc.42@gmail.com>
2023-04-11 15:04:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import JazzCard from "./components/JazzCard";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="">
+      <h1>Mon application React</h1>
+      <JazzCard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/JazzCard.jsx
@@ -0,0 +1,9 @@
+const JazzCard = () => {
+  return (
+    <div>
+      <img src="./src/assets/jazz_the_dog.jpg" alt="One dog in the kitchen" />
+    </div>
+  );
+};
+
+export default JazzCard;
