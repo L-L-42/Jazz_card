745c911aa636cb8a674baf8ed1cf2a74031af709
Luc <lefebureluc.42@gmail.com>
2023-04-12 23:38:58 +0200

    use framework Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import JazzCard from "./components/JazzCard";
 
-import "./App.css";
-
 const App = () => {
   return (
-    <div className="">
-      <h1>Mon application React</h1>
+    <div className=" mt-16 ml-8 max-md:mx-2xl: flex flex-col justify-center max-w-md rounded-md ">
+      <h1 className="p-2 font-bold text-2xl text-center bg-orange-700 mb-3 drop-shadow-2xl rounded-md ">
+        Jazz
+      </h1>
       <JazzCard />
+      <p className="  rounded-md mt-4 p-4 bg-orange-700 text-center font-bold pt-5">
+        This dog is an atypical animal. It tends to spin on itself.{" "}
+      </p>
     </div>
   );
 };
--- src/components/JazzCard.jsx
+++ src/components/JazzCard.jsx
@@ -1,7 +1,11 @@
 const JazzCard = () => {
   return (
     <div>
-      <img src="./src/assets/jazz_the_dog.jpg" alt="One dog in the kitchen" />
+      <img
+        className=" hover:animate-spin rounded-md"
+        src="./src/assets/jazz_the_dog.jpg"
+        alt="One dog in the kitchen"
+      />
     </div>
   );
 };
